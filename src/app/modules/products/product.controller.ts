f15f90433c976b0be3918116900f0a5d636c7994
alemranakash <alemranexpert@gmail.com>
2024-05-21 14:12:59 +0600

    feat: Implement 'Get all Product' route

--- src/app/modules/products/product.controller.ts
+++ src/app/modules/products/product.controller.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from "express";
 import { ProductServices } from "./movie.service";
 
+//* Post method to create a new product
 const createProduct = async (req: Request, res: Response) => {
 const productData = req.body;
 const result = await ProductServices.createProduct(productData);
-
-
-
     res.json({
       success: true,
       message: 'Product created successfully!',
@@ -14,7 +12,27 @@ const result = await ProductServices.createProduct(productData);
     });
   }
 
+//* Get method to get all products
+  const getAllProducts = async (req: Request, res: Response) => {
+  try{
+    const result = await ProductServices.getAllProducts();
+    res.status(200).json({
+      success: true,
+      message: 'Products fetched successfully!',
+      data: result,
+    });
+  } catch(err: any){
+    res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+      error: err,
+    });
+  }
+  }
+
+
 
   export const ProductController = {
     createProduct,
+    getAllProducts,
   }
--- src/app/modules/products/product.route.ts
+++ src/app/modules/products/product.route.ts
@@ -4,6 +4,10 @@ import { ProductController } from './product.controller';
 
 const router = express.Router();
 
+//* Post method to create a new product
 router.post('/', ProductController.createProduct);
+
+//* Get method to get all products
+router.get('/', ProductController.getAllProducts);
 
 export const ProductRoutes = router;
